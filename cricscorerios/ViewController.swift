78890451c7c6713f048ae9c21a93d0b2261f8f1d
Nat <nathanael.yip@gmail.com>
2019-10-26 12:18:49 +0800

    INIT

---
+++ cricscorerios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cricscorerios
+//
+//  Created by ctssstudent on 26/10/2019.
+//  Copyright © 2019 ctssstudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
