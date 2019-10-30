38e4ee8cff698d588226c81c214b2e6fb56aa11e
Dharshan <kpd2204@gmail.com>
2019-10-31 07:49:11 +0800

    Added another view controller and made the button working

---
+++ cricscorerios/MatchInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MatchInfoViewController.swift
+//  cricscorerios
+//
+//  Created by TemasekJC on 30/10/19.
+//  Copyright © 2019 ctssstudent. All rights reserved.
+//
+
+import UIKit
+
+class MatchInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
