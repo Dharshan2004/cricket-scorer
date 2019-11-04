19b326606331ddc9f1c1bbcc3e1c979f7faebbef
Dharshan <kpd2204@gmail.com>
2019-11-04 14:34:26 +0800

    scoreboard screen mostly done

---
+++ cricscorerios/ScoreboardViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ScoreboardViewController.swift
+//  cricscorerios
+//
+//  Created by TemasekJC on 4/11/19.
+//  Copyright © 2019 ctssstudent. All rights reserved.
+//
+
+import UIKit
+
+class ScoreboardViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var strikerLabel: UILabel!
+    @IBOutlet weak var nonStrikerLabel: UILabel!
+    @IBOutlet weak var bowlerLabel: UILabel!
+    @IBOutlet weak var crrLabel: UILabel!
+    @IBOutlet weak var strikerScoreLabel: UILabel!
+    @IBOutlet weak var nonStrikerScoreLabel: UILabel!
+    @IBOutlet weak var bowlerRunsLabel: UILabel!
+    @IBOutlet weak var striker4countLabel: UILabel!
+    @IBOutlet weak var nonStriker4Label: UILabel!
+    @IBOutlet weak var bowlerMaidenLabel: UILabel!
+    @IBOutlet weak var strikerBallLabel: UILabel!
+    @IBOutlet weak var nonStrikerBallLabel: UILabel!
+    @IBOutlet weak var ballsBowledLabel: UILabel!
+    @IBOutlet weak var rrrLabel: UILabel!
+    @IBOutlet weak var striker6Label: UILabel!
+    @IBOutlet weak var nonStriker6Label: UILabel!
+    @IBOutlet weak var bowlerWicketLabel: UIView!
+    @IBOutlet weak var strikerSRLabel: UIView!
+    @IBOutlet weak var nonStrikerSRLabel: UIView!
+    @IBOutlet weak var bowlerERLabel: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
